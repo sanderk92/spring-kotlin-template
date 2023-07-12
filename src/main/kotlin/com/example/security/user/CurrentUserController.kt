9641d95d01740631f7e7cae98e78bb29bf427fb3
Sander <sanderkrabbenborg@hotmail.com>
2023-07-12 22:48:35 +0200

    Unused import

--- src/main/kotlin/com/example/security/user/CurrentUserController.kt
+++ src/main/kotlin/com/example/security/user/CurrentUserController.kt
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
