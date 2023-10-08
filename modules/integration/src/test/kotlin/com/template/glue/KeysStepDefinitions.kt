5e9f64ba55825b9fb6d44434ceeb9e47766780f6
Sander <sanderkrabbenborg@hotmail.com>
2023-10-08 23:33:48 +0200

    Minor refactors

--- modules/integration/src/test/kotlin/com/template/glue/KeysStepDefinitions.kt
+++ modules/integration/src/test/kotlin/com/template/glue/KeysStepDefinitions.kt
@@ -2,37 +2,35 @@ package com.template.glue
 
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.fail
 import org.openapitools.client.apis.KeysApi
 import org.openapitools.client.models.ApiKeyCreateCommand
+import org.openapitools.client.models.ApiKeyView
 
 class KeysStepDefinitions(private val keysApi : KeysApi) {
 
     @When("creating api key with name {string}")
     fun whenCreatingApiKey(name: String) {
-        val command = ApiKeyCreateCommand(name = name, read = true, write = true, delete = true)
-        keysApi.createApiKey(command).block()
+        findApiKey(name)?.also { fail("api key with name '$name' must not already exist") }
+        keysApi.createApiKey(ApiKeyCreateCommand(name = name, read = true, write = true, delete = true)).block()!!
     }
 
     @When("deleting api key with name {string}")
     fun whenDeletingApiKey(name: String) {
-        val keys = keysApi.getApiKeys().block() ?: fail("could not retrieve api keys")
-        val key = keys.firstOrNull { it.name == name } ?: fail("api key with name $name was not found")
+        val key = findApiKey(name) ?: fail("api key with name '$name' must exist")
         keysApi.deleteApiKey(key.id).block()
-    }
-
-    @Then("api key with name {string} exists")
-    fun thenApiKeyExists(name: String) {
-        val keys = keysApi.getApiKeys().block() ?: fail("could not retrieve api keys")
-        val key = keys.firstOrNull { it.name == name }
-        assertThat(key).isNotNull
     }
 
     @Then("api key with name {string} does not exist")
     fun thenApiKeyNotExists(name: String) {
-        val keys = keysApi.getApiKeys().block() ?: fail("could not retrieve api keys")
-        val key = keys.firstOrNull { it.name == name }
-        assertThat(key).isNull()
+        findApiKey(name)?.also { fail("api key with name '$name' did exist: $it") }
     }
+
+    @Then("api key with name {string} exists")
+    fun thenApiKeyExists(name: String) {
+        findApiKey(name) ?: fail("api key with name '$name' did not exist")
+    }
+
+    private fun findApiKey(name: String): ApiKeyView? =
+        keysApi.getApiKeys().block()!!.firstOrNull { it.name == name }
 }
--- modules/integration/src/test/kotlin/com/template/glue/UserStepDefinitions.kt
+++ modules/integration/src/test/kotlin/com/template/glue/UserStepDefinitions.kt
@@ -3,14 +3,13 @@ package com.template.glue
 import com.template.objects.*
 import io.cucumber.java.en.Then
 import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.fail
 import org.openapitools.client.apis.UserApi
 
 class UserStepDefinitions(private val userApi : UserApi) {
 
     @Then("user details can be retrieved")
     fun thenRetrieveCurrentUser() {
-        val user = userApi.getCurrentUser().block() ?: fail("user details could not be retrieved")
+        val user = userApi.getCurrentUser().block()!!
         assertThat(user.firstName).isEqualTo(firstName)
         assertThat(user.lastName).isEqualTo(lastName)
         assertThat(user.email).isEqualTo(email)
@@ -18,7 +17,7 @@ class UserStepDefinitions(private val userApi : UserApi) {
 
     @Then("user can be found by first name")
     fun thenSearchUserByFirstName() {
-        val users = userApi.searchUsers(firstName).block() ?: fail("user could not be searched for")
+        val users = userApi.searchUsers(firstName).block()!!
         assertThat(users.size).isEqualTo(1)
         assertThat(users[0].firstName).isEqualTo(firstName)
         assertThat(users[0].lastName).isEqualTo(lastName)
@@ -27,7 +26,7 @@ class UserStepDefinitions(private val userApi : UserApi) {
 
     @Then("user can be found by last name")
     fun thenSearchUserByLastName() {
-        val users = userApi.searchUsers(firstName).block() ?: fail("user could not be searched for")
+        val users = userApi.searchUsers(firstName).block()!!
         assertThat(users.size).isEqualTo(1)
         assertThat(users[0].firstName).isEqualTo(firstName)
         assertThat(users[0].lastName).isEqualTo(lastName)
@@ -36,7 +35,7 @@ class UserStepDefinitions(private val userApi : UserApi) {
 
     @Then("user can be found by email")
     fun thenSearchUserByEmail() {
-        val users = userApi.searchUsers(firstName).block() ?: fail("user could not be searched for")
+        val users = userApi.searchUsers(firstName).block()!!
         assertThat(users.size).isEqualTo(1)
         assertThat(users[0].firstName).isEqualTo(firstName)
         assertThat(users[0].lastName).isEqualTo(lastName)
