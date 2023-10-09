236ab66c29e595afbab33b91745ef1e72fbbe95b
Sander <sanderkrabbenborg@hotmail.com>
2023-10-09 23:18:51 +0200

    Minor restructure

--- modules/core/src/main/kotlin/com/template/security/apikey/ApiKey.kt
+++ modules/core/src/main/kotlin/com/template/security/apikey/ApiKey.kt
@@ -10,3 +10,27 @@ interface ApiKey {
     val write: Boolean
     val delete: Boolean
 }
+
+sealed interface ApiKeyEntry {
+    val key: String
+    val name: String
+    val read: Boolean
+    val write: Boolean
+    val delete: Boolean
+}
+
+data class HashedApiKeyEntry(
+    override val key: String,
+    override val name: String,
+    override val read: Boolean,
+    override val write: Boolean,
+    override val delete: Boolean,
+) : ApiKeyEntry
+
+data class UnHashedApiKeyEntry(
+    override val key: String,
+    override val name: String,
+    override val read: Boolean,
+    override val write: Boolean,
+    override val delete: Boolean,
+) : ApiKeyEntry
--- modules/core/src/main/kotlin/com/template/security/apikey/ApiKeyService.kt
+++ modules/core/src/main/kotlin/com/template/security/apikey/ApiKeyService.kt
@@ -34,27 +34,3 @@ class ApiKeyService(private val hashGenerator: HashGenerator) {
         return apiKeyBuilder.toString()
     }
 }
-
-sealed interface ApiKeyEntry {
-    val key: String
-    val name: String
-    val read: Boolean
-    val write: Boolean
-    val delete: Boolean
-}
-
-data class HashedApiKeyEntry(
-    override val key: String,
-    override val name: String,
-    override val read: Boolean,
-    override val write: Boolean,
-    override val delete: Boolean,
-) : ApiKeyEntry
-
-data class UnHashedApiKeyEntry(
-    override val key: String,
-    override val name: String,
-    override val read: Boolean,
-    override val write: Boolean,
-    override val delete: Boolean,
-) : ApiKeyEntry
