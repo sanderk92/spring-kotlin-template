f73db3cfef256c8cb3d8408aeaf972d6a3d53431
Sander <sanderkrabbenborg@hotmail.com>
2023-12-16 02:29:02 +0100

    Minor improvement

--- modules/core/src/main/resources/db/migration/V1.1__apikeys.sql
+++ modules/core/src/main/resources/db/migration/V1.1__apikeys.sql
@@ -1,7 +1,7 @@
 CREATE TABLE apikeys
 (
   id          uuid          NOT NULL PRIMARY KEY,
-  owner       uuid          REFERENCES users,
+  owner       uuid          NOT NULL REFERENCES users,
   hashed_key  VARCHAR(255)  NOT NULL UNIQUE,
   name        VARCHAR(255)  NOT NULL UNIQUE
 );
@@ -9,7 +9,7 @@ CREATE TABLE apikeys
 CREATE TABLE apikey_authorities
 (
   api_key_entity_id  uuid  NOT NULL REFERENCES apikeys,
-  authorities        TEXT  CHECK (authorities IN ('READ', 'WRITE', 'DELETE', 'ADMIN'))
+  authorities        TEXT  NOT NULL CHECK (authorities IN ('READ', 'WRITE', 'DELETE', 'ADMIN'))
 );
 
 CREATE INDEX hashed_apikey ON apikeys (hashed_key)
