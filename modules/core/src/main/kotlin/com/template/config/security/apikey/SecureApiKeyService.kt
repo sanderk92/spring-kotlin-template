072e14a833cdae97b231f88bbba34d6ef0adffb3
Sander <sanderkrabbenborg@hotmail.com>
2024-04-29 21:13:45 +0200

    fix import

--- modules/core/src/main/kotlin/com/template/config/security/apikey/SecureApiKeyService.kt
+++ modules/core/src/main/kotlin/com/template/config/security/apikey/SecureApiKeyService.kt
@@ -1,7 +1,5 @@
 package com.template.config.security.apikey
 
-import com.template.config.security.apikey.SecureApiKey
-
 internal interface SecureApiKeyService {
     fun findByApiKey(hashedApiKey: String): SecureApiKey?
 }
